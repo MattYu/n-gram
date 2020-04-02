48cbc7be9141f99e6312e1471fbfad83af6b832a
MattYu <mingtyut@gmail.com>
2020-04-02 01:23:37 -0400

    implemented NaiveBayer Classifier

--- ngram.py
+++ ngram.py
@@ -1,5 +1,10 @@
 from collections import defaultdict
+import math as math
 
+from icu import LocalData
+
+
+ISALPHA_VOCABULARY_SIZE = 116766 
 
 def genAlphabetSet(V = 1):
     start = ord('a')
@@ -22,6 +27,7 @@ class NGramCell:
     def __init__(self, *args, **kwargs):
         self.dimension = kwargs.pop('dimension', 1)
         self.parent = kwargs.pop('parent', None)
+        self.char = kwargs.pop('char', "Root")
         self.nextChar = {}
         self.count = 0
 
@@ -30,51 +36,106 @@ class NGram:
         self.lan = kwargs.pop("lan")
         self.N = kwargs.pop("N")
         self.V = kwargs.pop('V', 1)
-        self.weight = kwargs.pop("weight", 0)
-        self.v = self.V
-        self.firstChar = {}
+        self.weight = kwargs.pop("weight", 0.001)
+        self.n = self.N
+        self.matrixRoot = NGramCell(dimension=0)
+        self.denominator = None
+        self.test = kwargs.pop("test", False)
+
         if self.V == 4:
             self.v = 3
 
         if self.V == 1 or self.V == 2:
             self.legalCharSet = genAlphabetSet(self.V)
+            self.vocabulary = len(self.legalCharSet)
+
+        if self.V == 3:
+            self.vocabulary = ISALPHA_VOCABULARY_SIZE
+
+        if self.V == 4:
+            self.vocabulary = ISALPHA_VOCABULARY_SIZE + 1
 
     def addTrainingInput(self, txt):
         splicedInput = self.spliceAndCleanInput(txt)
-        input = []
         for element in splicedInput:
             for j in range(0, len(element)):
-                stack = []
-                if element[j] not in self.firstChar:
-                    self.firstChar[element[j]] = NGramCell()
-                stack.append(self.firstChar[element[j]])
-                for i in range(j, min(j+ self.v, len(element))):
-                    stack[-1].count += 1
-
+                if element[j] not in self.matrixRoot.nextChar:
+                    self.matrixRoot.nextChar[element[j]] = NGramCell(char=element[j])
+                currentMatrixCell = self.matrixRoot.nextChar[element[j]]
+                for i in range(j, min(j+ self.n, len(element))):
+                    currentMatrixCell.count += 1
                     if i + 1 < len(element):
-                        if element[i+1] not in stack[-1].nextChar:
-                            stack[-1].nextChar[element[i+1]] = NGramCell(dimension=stack[-1].dimension+1, parent=stack[-1])
-                        stack.append(stack[-1].nextChar[element[i+1]])
+                        if element[i+1] not in currentMatrixCell.nextChar:
+                            currentMatrixCell.nextChar[element[i+1]] = NGramCell(char= element[i+1], dimension=currentMatrixCell.dimension+1, parent=currentMatrixCell)
+                        currentMatrixCell = currentMatrixCell.nextChar[element[i+1]]
+        if self.test:
+            stack = []
+            stack.append(self.matrixRoot)
+            print("*** Traing Ngram for: ")
+            print(splicedInput)
+            while len(stack) !=0:
+                current = stack.pop()
+                printStr = ""
+                for i in range (0, current.dimension):
+                    printStr += "\t"
+                printStr += current.char
+                printStr += " count: " + str(current.count)
+                print(printStr)
+                for element in current.nextChar:
+                    if current.nextChar[element].dimension <= self.N:
+                        stack.append(current.nextChar[element])
 
-        '''
-        for elem in self.firstChar:
-            print("************")
-            print(elem)
-            print(self.firstChar[elem].count)
 
-            for item in self.firstChar[elem].nextChar:
-                print("!!!!!")
-                print(item)
-                print(self.firstChar[elem].nextChar[item].count)
-                print("!!")
-                for i in self.firstChar[elem].nextChar[item].nextChar:
-                    print(i)
-                    print(self.firstChar[elem].nextChar[item].nextChar[i].count)
-                print("!!!!")
-            print("************")
-        
-        return
-        '''
+    
+    def getProbability(self, txt, newN=None, newWeight=None):
+        if newN != None:
+            self.N = newN
+        if newWeight != None:
+            self.weight = newWeight
+        splicedInput = self.spliceAndCleanInput(txt)
+        totalProbability = 0.0
+        self.denominator = None
+        for element in splicedInput:
+            for j in range(0, len(element)):
+                if self.N == 1:
+                    if element[j] not in self.matrixRoot.nextChar:
+                        numerator = self.weight*1.0
+                    else:
+                        numerator = self.matrixRoot.nextChar[element[j]].count + self.weight*1.0
+                    if self.denominator == None:
+                        self.denominator = 0.0
+                        for char in self.matrixRoot.nextChar:
+                            self.denominator += self.firstChar.count
+                    denominator = self.denominator + self.weight*math.pow(self.vocabulary, self.N)
+                    probability = numerator/denominator
+                    totalProbability += math.log10(probability)
+
+                if self.N > 1:
+                        currentMatrixCell = self.matrixRoot
+                        for i in range(j, j+ self.n):
+                            if i < len(element):
+                                if  element[i] not in currentMatrixCell.nextChar:
+                                    probability = self.weight*1.0/(self.weight*math.pow(self.vocabulary, self.N))
+                                    totalProbability += math.log10(probability)
+                                    currentMatrixCell = None
+                                    break
+                                else:
+                                    currentMatrixCell = currentMatrixCell.nextChar[element[i]]
+                            else:
+                                currentMatrixCell = None
+                                break
+  
+                        if currentMatrixCell != None:
+                            #print(currentMatrixCell.dimension)
+                            numerator = currentMatrixCell.count + self.weight*1.0
+                            denominator = currentMatrixCell.parent.count + self.weight*math.pow(self.vocabulary, self.N)
+                            probability = numerator/denominator
+
+                            totalProbability += math.log10(probability)
+                        
+                        # numerator = 
+        return totalProbability
+
 
     def spliceAndCleanInput(self, txt) -> [str]:
         ans = []
@@ -124,14 +185,18 @@ class NGram:
         #ans.append(txt[start:len(txt)])
         
         #print(self.legalCharSet)
-        print(ans)
+        #print(ans)
         return ans
 
 
 class MetaStatistics:
 
-    accuracy = 0
-    recall = 0
+    accuracy = 0.0
+    recall = 0.0
+    precision = 0.0
+    f_measure = 0.0
+    beta = 1.0
+    harmonic_mean = []
 
 class NaiveBayerClassifier(MetaStatistics):
 
@@ -141,28 +206,97 @@ class NaiveBayerClassifier(MetaStatistics):
     def __init__(self, *args, **kwargs):
         self.N = kwargs.pop('N', 3)
         self.V = kwargs.pop('V', 1)
+        self.weight = kwargs.pop("weight", 0.001)
         if "LANGUAGES" in kwargs:
             self.LANGUAGES = kwargs.pop('LANGUAGES')
     
         self.LanToNGrams= {}
 
         for lan in self.LANGUAGES:
-            self.LanToNGrams[lan] = NGram(lan = lan, N = self.N, V =self.V)
+            self.LanToNGrams[lan] = NGram(lan = lan, N = self.N, V =self.V, weight = self.weight)
 
-    def processInputLine(self, stringLine, training=True):
+    def addTrainingInputLine(self, stringLine):
+        lineList = stringLine.split("\t")
+        #id = lineList[0]
+        correctLan = lineList[2]
+        txt = lineList[3]
+
+        #print(id)
+        #print(correctLan)
+        #print(txt)
+
+        if correctLan in self.LanToNGrams:
+            self.LanToNGrams[correctLan].addTrainingInput(txt)
+
+    def trainFromTweets(self, fileName, maxLine = None):
+        with open(fileName, encoding="utf8") as f:
+            fil = f.read().splitlines()
+
+            if not maxLine:
+                for line in fil:
+                    self.addTrainingInputLine(line)
+            else:
+                for line in fil[:min(len(fil), maxLine)]:
+                    self.addTrainingInputLine(line)
+
+
+    def guessTweetLanguage(self, stringLine, logFile= None, newN= None, newWeight = None):
         lineList = stringLine.split("\t")
         id = lineList[0]
         correctLan = lineList[2]
         txt = lineList[3]
+        #print(id)
+        #print(correctLan)
+        #print(txt)
 
-        print(id)
-        print(correctLan)
-        print(txt)
+        guesses = []
 
-        if training:
-            if correctLan in self.LanToNGrams:
-                self.LanToNGrams[correctLan].addTrainingInput(txt)
+        for lan in self.LanToNGrams:
+            guesses.append([lan, self.LanToNGrams[lan].getProbability(txt, newN, newWeight)])
 
+        
+        guesses.sort(key= lambda x: x[1], reverse = True)
+
+
+        printStr = id + "  " + correctLan + "  " + str(guesses[0][1]) + "  " + guesses[0][0] + "  "
+
+        if guesses[0][0] == correctLan:
+            printStr += "correct"
+        else:
+            printStr += "wrong"
+        self.precision += 1
+        if correctLan != guesses[0][0]:
+            #print(guesses)
+            self.recall += 1
+            #print(printStr)
+
+
+    def runML(self, fileName, newN= None, newWeight = None, maxLine = None, resetStats = True):
+        if resetStats:
+            self.accuracy = 0.0
+            self.recall = 0.0
+            self.precision = 0.0
+            self.f_measure = 0.0
+            self.beta = 1.0
+            self.harmonic_mean = []
+
+        with open(fileName, encoding="utf8") as f:
+            fil = f.read().splitlines()
+
+            if not maxLine:
+                for line in fil:
+                    try:
+                        nb.guessTweetLanguage(line, newN=newN, newWeight=newWeight)
+                    except Exception as e:
+                        print(e)
+            else:
+                for line in fil[:min(len(fil), maxLine)]:
+                    try:
+                        nb.guessTweetLanguage(line,  newN=newN, newWeight=newWeight)
+                    except Exception as e:
+                        print(e)
+            print(self.recall)
+            print(self.precision)
 
 class HyperParameterOptimizer:
 # Perform on discrete value sweep using different hyperparameter to optimize the ML model
@@ -176,16 +310,12 @@ def processLine(stringLine):
     correctLan = lineList[2]
     txt = lineList[3]
 
-    print(id)
-    print(correctLan)
-    print(txt)
+    #print(id)
+    #print(correctLan)
+    #print(txt)
 
 # print(len(genAlphabetSet()))
-nb = NaiveBayerClassifier(V=3)
-with open('training-tweets.txt', encoding="utf8") as f:
-    fil = f.read().splitlines()
-
-    for line in fil[:1]:
-        nb.processInputLine(line)
-string = 'E'
-#print(string.isalpha())
+if __name__ == "__main__":
+    nb = NaiveBayerClassifier(N=3, V=4, weight=0.000005, test=False)
+    nb.trainFromTweets('training-tweets.txt')
+    nb.runML('test-tweets-given.txt')
